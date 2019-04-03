ac0151401ae5c3a4bbcb669dfc3f30c0be2fc7e9
Zara Burton <syzygyalstudio@gmail.com>
2019-04-03 10:54:28 +0100

    added csv with correct path!

--- flowerscript.py
+++ flowerscript.py
@@ -1,1 +1,26 @@
 #calculate mean mode and document steps in python for iris data set
+import pandas as pd 
+import numpy as np
+# reference for read csv https://www.datacamp.com/community/tutorials/pandas-read-csv
+df1 = pd.read_csv(r"C:\Users\Owner\Desktop\irisdataset/iristest.csv")
+print(df1.head(5))
+
+
+#import pandas as pd
+#import numpy as numpy
+#df = pd.read_csv(r"C:\Users\Owner\Desktop\IRISTOO\iristest.csv")
+#print(df.head(5)) finding the path was a nightmare - kept on throwing file not found because I wrote the wrong thing like a numpty
+
+
+
+#https://www.kaggle.com/jchen2186/machine-learning-with-iris-dataset
+#data = pd.read_csv("\Desktop\iristest.csv")
+     #adapted from shawnlynn https://www.shanelynn.ie/python-pandas-read_csv-load-data-from-csv-files/
+#print(data.head())
+
+#import pandas as pd
+#ocation = ("\irisdataset\iristest.csv")
+
+#df = pd.read_csv("/Users/Desktop/irisdataset/iristest.csv")
+#df = pd.read_csv(
+
